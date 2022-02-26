06eb84f22a96e5befaf3b6b3ab9d0418a6db798b
cg4079 <98930852+cg4079@users.noreply.github.com>
2022-02-26 13:50:10 -0700

    Add files via upload

--- solution.py
+++ solution.py
@@ -6,30 +6,31 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google Mail server) if you want to verify the script beyond GradeScope
-
+    mailserver = "mail.google.com"
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
-    clientSocket = socket((mail.google.com, 25))
-    recv = clientSocket.recv(1024).decode()
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, 25))
     # Fill in end
 
-    #print(recv) #You can use these print statement to validate return codes from the server.
-    #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+    recv = clientSocket.recv(1024).decode()
+    print(
+        recv) #You can use these print statement to validate return codes from the server.
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Carlos\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-
-    #print(recv1)
-    #if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
+    print(recv1)
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    clientSocket.sendall(('MAIL FROM: <'+carlos.garduno2011@gmail.com+'\r\n'))
+    clientSocket.sendall(('MAIL FROM: <'+carlos.garduno2011@gmail.com+'\r\n').encode())
     recv2 = clientSocket.recv(1024).decode()
     print(recv2)
     if recv2[:3] != '250':
@@ -79,5 +80,5 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
 
-if __name__ == '__main__':
-    smtp_client(1025, '127.0.0.1')
+#if __name__ == '__main__':
+#    smtp_client(1025, '127.0.0.1')
